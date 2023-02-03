b2b3932535bd11a4189a167859fa0a11417eb04a
digaogalo <digaolimac@gmail.com>
2023-02-03 17:04:47 -0300

    async.json api

--- js/script.js
+++ js/script.js
@@ -6,5 +6,25 @@ async function getPeople() {
 
     return response.json()
 }
-//
-getPeople().then( data => console.log( data))
+
+// filtrar somente mulheres 
+getPeople().then( data => {
+    const people = data.results
+   // console.log(people.filter(p => p.gender === 'male' ))
+})
+
+//Trabalhando com dados 
+// esses dados vão filtrar somente users com mais de 40 anos
+getPeople().then( data => {
+    const result = data.results.filter(p => p.dob.age >= 30)
+    const people = []
+    for (let p of result) {
+        people.push({
+            'Nome' :`${p.name.first} ${p.name.last}`,
+            'Sexo' : p.gender ,
+            'Idade' : p.dob.age
+        })
+    }
+
+    console.table(people)
+})
